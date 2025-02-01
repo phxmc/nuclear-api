9d922f129c6c5cf3ddb0b58e98456bb24b633327
orewaee <orewaee@gmail.com>
2025-02-01 20:46:38 +0400

    fix: replace string with json response

--- internal/middlewares/auth.go
+++ internal/middlewares/auth.go
@@ -3,10 +3,10 @@ package middlewares
 import (
 	"fmt"
 	"github.com/orewaee/nuclear-api/internal/app/api"
+	"github.com/orewaee/nuclear-api/internal/dto"
 	"github.com/orewaee/nuclear-api/internal/utils"
 	"github.com/orewaee/typedenv"
 	"github.com/valyala/fasthttp"
-	"net/http"
 	"strconv"
 	"strings"
 )
@@ -15,42 +15,49 @@ func Auth(authApi api.AuthApi, handler fasthttp.RequestHandler) fasthttp.Request
 	return func(ctx *fasthttp.RequestCtx) {
 		header := string(ctx.Request.Header.Peek("Authorization"))
 		if header == "" {
-			utils.MustWriteString(ctx, "missing authorization header", fasthttp.StatusUnauthorized)
+			response := &dto.Error{Message: "missing authorization header"}
+			utils.MustWriteJson(ctx, response, fasthttp.StatusUnauthorized)
 			return
 		}
 
 		accessToken := strings.TrimPrefix(header, "Bearer ")
 		if accessToken == "" {
-			utils.MustWriteString(ctx, "missing token", fasthttp.StatusUnauthorized)
+			response := &dto.Error{Message: "missing token"}
+			utils.MustWriteJson(ctx, response, fasthttp.StatusUnauthorized)
 			return
 		}
 
 		if accessToken == "" {
-			utils.MustWriteString(ctx, "missing token", http.StatusUnauthorized)
+			response := &dto.Error{Message: "missing token"}
+			utils.MustWriteJson(ctx, response, fasthttp.StatusUnauthorized)
 			return
 		}
 
 		claims, err := authApi.GetTokenClaims(accessToken, typedenv.String("ACCESS_KEY"))
 		if err != nil {
-			utils.MustWriteString(ctx, "invalid token", http.StatusUnauthorized)
+			response := &dto.Error{Message: "invalid token"}
+			utils.MustWriteJson(ctx, response, fasthttp.StatusUnauthorized)
 			return
 		}
 
 		permsClaim, ok := claims["perms"]
 		if !ok {
-			utils.MustWriteString(ctx, "invalid token", http.StatusUnauthorized)
+			response := &dto.Error{Message: "invalid token"}
+			utils.MustWriteJson(ctx, response, fasthttp.StatusUnauthorized)
 			return
 		}
 
 		perms, err := strconv.Atoi(fmt.Sprintf("%v", permsClaim))
 		if err != nil {
-			utils.MustWriteString(ctx, "invalid token", http.StatusUnauthorized)
+			response := &dto.Error{Message: "invalid token"}
+			utils.MustWriteJson(ctx, response, fasthttp.StatusUnauthorized)
 			return
 		}
 
 		emailClaim, ok := claims["email"]
 		if !ok {
-			utils.MustWriteString(ctx, "invalid token", http.StatusUnauthorized)
+			response := &dto.Error{Message: "invalid token"}
+			utils.MustWriteJson(ctx, response, fasthttp.StatusUnauthorized)
 			return
 		}
 
