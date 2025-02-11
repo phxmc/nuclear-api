4255c6cfb628b571749c4a5c5e1ec64b4a17f884
orewaee <orewaee@gmail.com>
2025-02-11 20:57:49 +0400

    refactor: minor refactoring of cors middleware

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -1,20 +1,28 @@
 package middlewares
 
-import "github.com/valyala/fasthttp"
+import (
+	"github.com/valyala/fasthttp"
+	"strings"
+)
 
 var (
-	corsAllowHeaders     = "*"
-	corsAllowMethods     = "*"
-	corsAllowOrigin      = "*"
+	corsAllowHeaders     = []string{"Content-Type", "Authorization"}
+	corsAllowMethods     = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	corsAllowOrigin      = []string{"http://localhost:3000"}
 	corsAllowCredentials = "true"
 )
 
 func Cors(next fasthttp.RequestHandler) fasthttp.RequestHandler {
 	return func(ctx *fasthttp.RequestCtx) {
-		ctx.Response.Header.Set("Access-Control-Allow-Credentials", corsAllowCredentials)
-		ctx.Response.Header.Set("Access-Control-Allow-Headers", corsAllowHeaders)
-		ctx.Response.Header.Set("Access-Control-Allow-Methods", corsAllowMethods)
-		ctx.Response.Header.Set("Access-Control-Allow-Origin", corsAllowOrigin)
+		setHeader := func(key string, values []string) {
+			ctx.Response.Header.Set(key, strings.Join(values, ","))
+		}
+
+		setHeader(fasthttp.HeaderAccessControlAllowOrigin, corsAllowOrigin)
+		setHeader(fasthttp.HeaderAccessControlAllowMethods, corsAllowMethods)
+		setHeader(fasthttp.HeaderAccessControlAllowHeaders, corsAllowHeaders)
+
+		ctx.Response.Header.Set(fasthttp.HeaderAccessControlAllowCredentials, corsAllowCredentials)
 
 		next(ctx)
 	}
