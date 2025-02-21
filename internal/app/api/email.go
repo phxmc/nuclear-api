3e00bfbef284b27cdf435ac813dc324af37ec20f
orewaee <orewaee@gmail.com>
2025-02-21 23:50:04 +0400

    fix: fix the layout and message format

--- internal/app/api/email.go
+++ internal/app/api/email.go
@@ -2,10 +2,11 @@ package api
 
 import (
 	"context"
-	"github.com/orewaee/nuclear-api/internal/app/domain"
 )
 
 type EmailApi interface {
 	Send(ctx context.Context, receiver, subject, text string) error
-	SendRegisterMail(ctx context.Context, receiver string, tempAccount *domain.TempAccount) error
+
+	SendLoginEmail(ctx context.Context, receiver, device, datetime, code string) error
+	SendRegisterEmail(ctx context.Context, receiver, device, datetime, code string) error
 }
